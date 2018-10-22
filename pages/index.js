ce3e95bd21eb69f4c4a6f464335a67e7c1fef6b5
evadb <evademersbrett@gmail.com>
2018-10-22 14:18:14 -0400

    First commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+class App extends Component {
+  render() {
+    return <div>Welcome to Mr Pinchy&apos;s Confession Box</div>
+  }
+}
+
+export default App
